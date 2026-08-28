ded390a8173e370eeebccb54a920dff54027f63f
iman2604siddiqui <imansiddiqui2604@gmail.com>
2026-08-28 22:33:49 +0500

    Fix server listening on specified PORT

--- server.js
+++ server.js
@@ -173,3 +173,4 @@ app.get('/api/escalations', auth, requireRole('hr_admin', 'it_support'), async (
 
 await fs.mkdir(storageDir, { recursive: true });
 app.listen(port, () => console.log(`Keycard API listening on http://localhost:${port}`));
+app.listen(PORT, () =>{} )
